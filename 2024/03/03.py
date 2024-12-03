e2ccb6b173bd54b3e30eacd5aa6c3bb5733b18b1
samodostal <samuel.dostal.sk@gmail.com>
2024-12-03 08:23:10 +0100

    refactor day 03

--- 2024/03/03.py
+++ 2024/03/03.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import math
 
 EXAMPLE_DATA = """
 """.strip()
@@ -16,17 +17,14 @@ def main():
     )
 
     # Part 1
-    commands_p1 = [n for n in re.findall(r"mul\(\d{1,3},\d{1,3}\)", data)]
+    commands_p1 = re.findall(r"mul\(\d{1,3},\d{1,3}\)", data)
     for command in commands_p1:
-        [x, y] = [int(n) for n in re.findall(r"-?\d+", command)]
-        answer1 += x * y
+        answer1 += math.prod([int(n) for n in re.findall(r"-?\d+", command)])
 
     # Part 2
     multiply = True
 
-    commands_p2 = [
-        n for n in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)", data)
-    ]
+    commands_p2 = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)", data)
     for command in commands_p2:
         if command == "do()":
             multiply = True
@@ -39,8 +37,7 @@ def main():
         if not multiply:
             continue
 
-        [x, y] = [int(n) for n in re.findall(r"-?\d+", str(command))]
-        answer2 += x * y
+        answer2 += math.prod([int(n) for n in re.findall(r"-?\d+", str(command))])
 
     print(answer1)
     print(answer2)
--- 2024/template.py
+++ 2024/template.py
@@ -1,4 +1,5 @@
 import sys
+import math
 import re
 
 EXAMPLE_DATA = """
